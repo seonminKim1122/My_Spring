21492c775fb4544ecc4eac1f3c0a780e6a14314b
Kim Seonmin <w.hand715@gmail.com>
2024-01-15 21:31:35 +0900

    Practice API that returns Object

--- spring_introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring_introduction/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,34 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 의 body 에 리턴값을 넣겠다 = return 값과 동일한 이름의 html 이 있는지 templates 에서 찾지 않음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // API 의 응답은 보통 JSON 타입
+    // Spring 에서는 Object 를 Return 하면 자동으로 Json 으로 변환해서 클라이언트로 내려줌
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
