a18dc36ca11aa6a323802ead269911e77ed4d7f8
김선민 <w.hand715@gmail.com>
2024-02-01 13:26:54 +0900

    apply AOP

--- spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,11 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -37,6 +35,11 @@ public class SpringConfig {
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
+    }
+
+    @Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
     }
 
 //    @Bean
