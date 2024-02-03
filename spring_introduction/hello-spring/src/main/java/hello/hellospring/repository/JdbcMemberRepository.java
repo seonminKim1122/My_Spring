09a1aa4431c9af05a4e5521f2943d418ed10262d
Kim Seonmin <w.hand715@gmail.com>
2024-02-03 16:08:43 +0900

    solve conflict

--- spring_introduction/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ spring_introduction/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -5,16 +5,7 @@ import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
 import java.sql.*;
-<<<<<<< HEAD
-import java.util.List;
-import java.util.Optional;
 
-public class JdbcMemberRepository implements MemberRepository{
-
-    private DataSource dataSource;
-
-    public JdbcMemberRepository(DataSource dataSource) {
-=======
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,33 +15,17 @@ public class JdbcMemberRepository implements MemberRepository {
     private final DataSource dataSource; // DB 에 접근하기 위해 필요, Spring Container 를 통해 DI 가능
 
     public JdbcMemberRepository(DataSource dataSource) { // Spring Container 로부터 주입 받아야 함
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         this.dataSource = dataSource;
     }
 
     @Override
     public Member save(Member member) {
-<<<<<<< HEAD
-//        String sql = "insert into  member(name) values(?)";
-        String sql = "";
-=======
         String sql = "insert into member (name) values (?)"; // Unable to resolve table 'member' -> 어플리케이션 실행에 문제 없음
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         try {
-<<<<<<< HEAD
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-            pstmt.setString(1, member.getName());
-
-            pstmt.executeUpdate();
-            rs = pstmt.getGeneratedKeys();
-
-=======
             conn = getConnection();
 
             pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -59,16 +34,12 @@ public class JdbcMemberRepository implements MemberRepository {
             
             // Member 객체의 id 값 셋팅을 위해서 DB 에서 생성된 id 가 필요
             rs = pstmt.getGeneratedKeys();
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
+
             if (rs.next()) {
                 member.setId(rs.getLong(1));
             } else {
                 throw new SQLException("id 조회 실패");
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
             return member;
         } catch (Exception e) {
             throw new IllegalStateException(e);
@@ -79,9 +50,6 @@ public class JdbcMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-<<<<<<< HEAD
-        return Optional.empty();
-=======
         String sql = "select * from member where id = ?";
 
         Connection conn = null;
@@ -109,15 +77,10 @@ public class JdbcMemberRepository implements MemberRepository {
         } finally {
             close(conn, pstmt, rs);
         }
-
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-<<<<<<< HEAD
-        return Optional.empty();
-=======
         String sql = "select * from member where name = ?";
 
         Connection conn = null;
@@ -146,22 +109,10 @@ public class JdbcMemberRepository implements MemberRepository {
         } finally {
             close(conn, pstmt, rs);
         }
-
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
     }
 
     @Override
     public List<Member> findAll() {
-<<<<<<< HEAD
-        return null;
-    }
-
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-=======
         String sql = "select * from member";
 
         Connection conn = null;
@@ -191,47 +142,33 @@ public class JdbcMemberRepository implements MemberRepository {
         }
     }
 
+
     private Connection getConnection() {
         return DataSourceUtils.getConnection(dataSource);
     }
+
     private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
         try {
             if (rs != null) rs.close();
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         try {
-<<<<<<< HEAD
-            if (pstmt != null) {
-                pstmt.close();
-            }
-=======
             if (pstmt != null) pstmt.close();
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         try {
-<<<<<<< HEAD
-            if (conn != null) {
-                close(conn);
-            }
-=======
             if (conn != null) close(conn);
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     private void close(Connection conn) throws SQLException {
-<<<<<<< HEAD
-=======
         // DataSourceUtils?
->>>>>>> 32a32180a0e1b25daeca3e6b2e812a1569023eed
         DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
