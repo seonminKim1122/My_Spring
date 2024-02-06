1ed5652429de795bc26c05faa5e36ad96cef3fd8
김선민 <w.hand715@gmail.com>
2024-02-06 11:53:51 +0900

    modify test code

--- spring_core_basic/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring_core_basic/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -6,13 +6,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     @Test
     void createOrder() {
         Long memberId = 1L;
