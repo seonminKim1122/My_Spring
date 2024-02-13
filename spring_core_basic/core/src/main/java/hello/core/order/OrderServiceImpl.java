8dcc47b7da8a732a876340e6d6dc7a2def3c55c7
Kim Seonmin <w.hand715@gmail.com>
2024-02-14 00:17:53 +0900

    apply lombok

--- spring_core_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring_core_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,40 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 키워드가 붙은 멤버 변수들로 구성된 생성자 자동으로 만들어 줌 -> 스프링은 생성자가 하나면 @Autowired 생략 가능 -> 이거 하나로 의존관계 자동 주입까지 끝
 public class OrderServiceImpl implements OrderService {
 
-//    @Autowired
     private final MemberRepository memberRepository;
-//    @Autowired
     private final DiscountPolicy discountPolicy;
-
-    /*
-    수정자 주입이나 필드 주입 방식은 빈 등록 단계, 의존관계 주입 단계로 나눌 수 있지만
-    생성자 주입 방식은 빈을 생성하기 위해 생성자를 호출하게 되므로 생성과 의존관계 주입이 동시에 일어난다고 본다
-    생성자 주입, 수정자 주입, 필드 주입 다 이용하고 있다면 다 호출된다.
-    */
-
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    @Autowired // 생성자가 하나만 있으면 Autowired 를 생략해도 스프링 빈은 자동으로 의존관계 주입이 된다
-    // @Autowired 는 required = true 가 default 라 주입할 빈이 없으면 오류가 난다
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
