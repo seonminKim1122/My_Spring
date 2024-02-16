d2efb3bb6bc733c6c1b04275be80f8981f8e490d
김선민 <w.hand715@gmail.com>
2024-02-16 09:33:15 +0900

    BeanLifeCycle Callback with Annotation

--- spring_core_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring_core_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 //import org.springframework.beans.factory.DisposableBean;
 //import org.springframework.beans.factory.InitializingBean;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -33,6 +36,7 @@ public class NetworkClient {
     // 인터페이스로 초기화, 소멸 콜백
     // 단점 : 스프링에 의존적. 그래서 지금은 거의 사용하지 않는 방법임
 
+    @PostConstruct
     public void init() throws Exception {
         // 의존관계 주입이 끝나면 스프링이 이 메서드 자동으로 호출(초기화 콜백)
         System.out.println("NetworkClient.init");
@@ -40,7 +44,7 @@ public class NetworkClient {
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() throws Exception {
         // 소멸 전 콜백(스프링이 내려가기 전에 모든 빈을 삭제하게 되는데 이 직전 시점에 호출)
         System.out.println("NetworkClient.close");
